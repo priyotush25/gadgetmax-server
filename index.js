8a02acec312195a3968dfe027d0c9cfbbdf766cb
Priyotush <priyotushpaul40@gmail.com>
2026-01-21 00:18:09 +0600

    update all

--- index.js
+++ index.js
@@ -151,10 +151,10 @@ app.post("/api/items", (req, res) => {
   res.status(201).json(newItem);
 });
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
+// if (process.env.NODE_ENV !== "production") {
+//   app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+//   });
+// }
 
 module.exports = app;
